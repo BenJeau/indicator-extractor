9d737598032f10579c6365575cd6646abe26f1c0
Benoit Jeaurond <benoit@jeaurond.dev>
2024-08-26 19:06:37 -0400

    feat: add hash parsing support

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,12 @@
 use std::net::{Ipv4Addr, Ipv6Addr};
 
-use nom::{branch::alt, combinator::opt, IResult};
+use nom::{
+    branch::alt,
+    bytes::complete::take,
+    combinator::opt,
+    error::{make_error, ErrorKind},
+    Err, IResult,
+};
 
 enum Indicator {
     Url(String),
@@ -52,18 +58,46 @@ impl Indicator {
     }
 
     fn extract_sha512(input: &str) -> IResult<&str, Self> {
-        todo!()
+        let (input, sha512) = take(128usize)(input)?;
+
+        if Self::all_hex(sha512) {
+            Ok((input, Self::Sha512(sha512.to_lowercase())))
+        } else {
+            Err(Err::Error(make_error(input, ErrorKind::Verify)))
+        }
     }
 
     fn extract_sha256(input: &str) -> IResult<&str, Self> {
-        todo!()
+        let (input, sha256) = take(64usize)(input)?;
+
+        if Self::all_hex(sha256) {
+            Ok((input, Self::Sha256(sha256.to_lowercase())))
+        } else {
+            Err(Err::Error(make_error(input, ErrorKind::Verify)))
+        }
     }
 
     fn extract_sha1(input: &str) -> IResult<&str, Self> {
-        todo!()
+        let (input, sha1) = take(40usize)(input)?;
+
+        if Self::all_hex(sha1) {
+            Ok((input, Self::Sha1(sha1.to_lowercase())))
+        } else {
+            Err(Err::Error(make_error(input, ErrorKind::Verify)))
+        }
     }
 
     fn extract_md5(input: &str) -> IResult<&str, Self> {
-        todo!()
+        let (input, md5) = take(32usize)(input)?;
+
+        if Self::all_hex(md5) {
+            Ok((input, Self::Md5(md5.to_lowercase())))
+        } else {
+            Err(Err::Error(make_error(input, ErrorKind::Verify)))
+        }
+    }
+
+    fn all_hex(input: &str) -> bool {
+        input.chars().all(|c| c.is_ascii_hexdigit())
     }
 }
