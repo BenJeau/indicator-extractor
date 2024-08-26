a08c2d72fff21ec0d29fa6ef3c854f48aa09c9f3
Benoit Jeaurond <benoit@jeaurond.dev>
2024-08-26 19:09:58 -0400

    feat: add tests for hash parsing

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -8,6 +8,7 @@ use nom::{
     Err, IResult,
 };
 
+#[derive(Debug, PartialEq)]
 enum Indicator {
     Url(String),
     Email(String),
@@ -42,19 +43,19 @@ impl Indicator {
     }
 
     fn extract_url(input: &str) -> IResult<&str, Self> {
-        todo!()
+        Err(Err::Error(make_error(input, ErrorKind::Verify)))
     }
 
     fn extract_email(input: &str) -> IResult<&str, Self> {
-        todo!()
+        Err(Err::Error(make_error(input, ErrorKind::Verify)))
     }
 
     fn extract_ipv4(input: &str) -> IResult<&str, Self> {
-        todo!()
+        Err(Err::Error(make_error(input, ErrorKind::Verify)))
     }
 
     fn extract_ipv6(input: &str) -> IResult<&str, Self> {
-        todo!()
+        Err(Err::Error(make_error(input, ErrorKind::Verify)))
     }
 
     fn extract_sha512(input: &str) -> IResult<&str, Self> {
@@ -99,5 +100,50 @@ impl Indicator {
 
     fn all_hex(input: &str) -> bool {
         input.chars().all(|c| c.is_ascii_hexdigit())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_md5() {
+        let input = "MD5 hash: d41d8cd98f00b204e9800998ecf8427e";
+        let expected = vec![Indicator::Md5(
+            "d41d8cd98f00b204e9800998ecf8427e".to_string(),
+        )];
+
+        assert_eq!(Indicator::extract_indicators(input), expected);
+    }
+
+    #[test]
+    fn test_extract_sha1() {
+        let input = "SHA1 hash: da39a3ee5e6b4b0d3255bfef95601890afd80709";
+        let expected = vec![Indicator::Sha1(
+            "da39a3ee5e6b4b0d3255bfef95601890afd80709".to_string(),
+        )];
+
+        assert_eq!(Indicator::extract_indicators(input), expected);
+    }
+
+    #[test]
+    fn test_extract_sha256() {
+        let input = "SHA256 hash: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+        let expected = vec![Indicator::Sha256(
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855".to_string(),
+        )];
+
+        assert_eq!(Indicator::extract_indicators(input), expected);
+    }
+
+    #[test]
+    fn test_extract_sha512() {
+        let input = "SHA512 hash: f1d9d8f153ec808a44cd63fb85f7cb811845b1596e46e78febd8c8b505a9b7d3a242c98b2b51261e5402f37334beefd7ba4066873a6dc56cd030cf29f4aef6dc";
+        let expected = vec![Indicator::Sha512(
+            "f1d9d8f153ec808a44cd63fb85f7cb811845b1596e46e78febd8c8b505a9b7d3a242c98b2b51261e5402f37334beefd7ba4066873a6dc56cd030cf29f4aef6dc".to_string(),
+        )];
+
+        assert_eq!(Indicator::extract_indicators(input), expected);
     }
 }
