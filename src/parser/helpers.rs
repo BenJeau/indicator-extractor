c0254016b904b7989af213a3d76109c0552cac42
Benoit Jeaurond <benoit@jeaurond.dev>
2024-08-26 22:46:55 -0400

    feat: parse ipv6 addresses and emails

---
+++ src/parser/helpers.rs
@@ -0,0 +1,41 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::{complete::digit1, is_digit, is_hex_digit},
+    error::{make_error, ErrorKind},
+    number::complete::hex_u32,
+    Err, IResult,
+};
+
+pub(crate) fn hex_u16(input: &[u8]) -> IResult<&[u8], u16> {
+    let (input, data) = hex_u32(input)?;
+    Ok((input, data as u16))
+}
+
+pub(crate) fn dec_u8(input: &[u8]) -> IResult<&[u8], u8> {
+    let (input, data) = digit1(input)?;
+    let octect_string =
+        std::str::from_utf8(data).map_err(|_| Err::Error(make_error(input, ErrorKind::Verify)))?;
+    Ok((
+        input,
+        octect_string
+            .parse()
+            .map_err(|_| Err::Error(make_error(input, ErrorKind::Verify)))?,
+    ))
+}
+
+pub(crate) fn defanged_period(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    alt((tag("."), tag("[.]")))(input)
+}
+
+pub(crate) fn defanged_colon(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    alt((tag(":"), tag("[:]")))(input)
+}
+
+pub(crate) fn is_not_digit(c: u8) -> bool {
+    !is_digit(c)
+}
+
+pub(crate) fn is_not_hex_digit(c: u8) -> bool {
+    !is_hex_digit(c)
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,19 +1,20 @@
-use std::net::{Ipv4Addr, Ipv6Addr};
-
+use helpers::{dec_u8, defanged_colon, defanged_period, hex_u16, is_not_digit, is_not_hex_digit};
 use nom::{
     branch::alt,
     bytes::complete::{is_not, tag, take_while},
     character::{
-        complete::{hex_digit1, multispace0},
-        is_digit,
-        streaming::multispace1,
+        complete::{alphanumeric1, hex_digit1, multispace0, multispace1},
+        is_alphanumeric,
     },
     combinator::{complete, opt},
     error::{make_error, ErrorKind},
-    multi::separated_list0,
+    multi::{many1, separated_list0, separated_list1},
     sequence::preceded,
     Err, IResult,
 };
+use std::net::{Ipv4Addr, Ipv6Addr};
+
+mod helpers;
 
 #[derive(Debug, PartialEq)]
 pub enum Indicator<'a> {
@@ -30,7 +31,7 @@ pub enum Indicator<'a> {
 pub fn extract_indicators(input: &[u8]) -> IResult<&[u8], Vec<Indicator>> {
     let (input, _) = multispace0(input)?;
     let (input, indicator) = complete(separated_list0(
-        preceded(is_not(" \t\r\n"), multispace1),
+        preceded(opt(is_not(" \t\r\n")), multispace1),
         opt(extract_indicator),
     ))(input)?;
     Ok((input, indicator.into_iter().flatten().collect()))
@@ -51,44 +52,60 @@ fn extract_url(input: &[u8]) -> IResult<&[u8], Indicator> {
 }
 
 fn extract_email(input: &[u8]) -> IResult<&[u8], Indicator> {
-    Err(Err::Error(make_error(input, ErrorKind::Verify)))
+    let (input, _) = opt(take_while(|c| {
+        c != b'.' && c != b'-' && c != b'_' && c != b'+' && !is_alphanumeric(c)
+    }))(input)?;
+
+    let (input, email) =
+        many1(alt((alphanumeric1, tag("."), tag("-"), tag("_"), tag("+"))))(input)?;
+    let (input, _) = tag("@")(input)?;
+    let (input, first_part) = many1(alt((alphanumeric1, tag("-"))))(input)?;
+    let (input, domain) = preceded(defanged_period, many1(alt((alphanumeric1, tag("-")))))(input)?;
+
+    Ok((
+        input,
+        Indicator::Email(format!(
+            "{}@{}.{}",
+            std::str::from_utf8(&email.concat()).unwrap(),
+            std::str::from_utf8(&first_part.concat()).unwrap(),
+            std::str::from_utf8(&domain.concat()).unwrap()
+        )),
+    ))
 }
 
 fn extract_ipv4(input: &[u8]) -> IResult<&[u8], Indicator> {
-    let (input, first_octet) = octect(input)?;
-    let (input, _) = eat_defanged_period(input)?;
-    let (input, second_octet) = octect(input)?;
-    let (input, _) = eat_defanged_period(input)?;
-    let (input, third_octet) = octect(input)?;
-    let (input, _) = eat_defanged_period(input)?;
-    let (input, fourth_octet) = octect(input)?;
+    let (input, _) = opt(take_while(is_not_digit))(input)?;
 
-    let ipv4_addr = Ipv4Addr::new(first_octet, second_octet, third_octet, fourth_octet);
+    let (input, octects) = separated_list1(defanged_period, dec_u8)(input)?;
+
+    if octects.len() != 4 {
+        return Err(Err::Error(make_error(input, ErrorKind::Verify)));
+    }
+
+    let ipv4_addr = Ipv4Addr::new(octects[0], octects[1], octects[2], octects[3]);
 
     Ok((input, Indicator::Ipv4(ipv4_addr)))
 }
 
-fn octect(input: &[u8]) -> IResult<&[u8], u8> {
-    let (input, octet_parts) = take_while(is_digit)(input)?;
-    Ok((
-        input,
-        octet_parts
-            .iter()
-            .rev()
-            .enumerate()
-            .fold(0, |acc, (i, c)| acc + 10_u8.pow(i as u32) * (c - b'0')),
-    ))
-}
-
-fn eat_defanged_period(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    alt((tag("."), tag("[.]")))(input)
-}
-
 fn extract_ipv6(input: &[u8]) -> IResult<&[u8], Indicator> {
-    Err(Err::Error(make_error(input, ErrorKind::Verify)))
+    let (input, _) = opt(take_while(|c| c != b':' && !is_alphanumeric(c)))(input)?;
+
+    let (input, hexes) = separated_list1(defanged_colon, hex_u16)(input)?;
+
+    if hexes.len() != 8 {
+        return Err(Err::Error(make_error(input, ErrorKind::Verify)));
+    }
+
+    let ipv6_addr = Ipv6Addr::new(
+        hexes[0], hexes[1], hexes[2], hexes[3], hexes[4], hexes[5], hexes[6], hexes[7],
+    );
+
+    Ok((input, Indicator::Ipv6(ipv6_addr)))
 }
 
 fn extract_hash(input: &[u8]) -> IResult<&[u8], Indicator> {
+    let (input, _) = opt(take_while(is_not_hex_digit))(input)?;
+
     let (input, hash) = hex_digit1(input)?;
 
     match hash.len() {
@@ -103,6 +120,17 @@ fn extract_hash(input: &[u8]) -> IResult<&[u8], Indicator> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_extract_single_ipv4_with_garbage() {
+        let input = "asdf127.0.0.1";
+        let expected = Indicator::Ipv4(Ipv4Addr::new(127, 0, 0, 1));
+
+        assert_eq!(
+            extract_indicator(input.as_bytes()),
+            Ok(("".as_bytes(), expected))
+        );
+    }
 
     #[test]
     fn test_extract_single_ipv4() {
@@ -130,6 +158,19 @@ mod tests {
     fn test_extract_single_fully_defanged_ipv4() {
         let input = "127[.]0[.]0[.]1";
         let expected = Indicator::Ipv4(Ipv4Addr::new(127, 0, 0, 1));
+
+        assert_eq!(
+            extract_indicator(input.as_bytes()),
+            Ok(("".as_bytes(), expected))
+        );
+    }
+
+    #[test]
+    fn test_extract_single_partially_defanged_ipv6() {
+        let input = "2001:0db8[:]85a3[:]0000:0000[:]8a2e:0370:7334";
+        let expected = Indicator::Ipv6(Ipv6Addr::new(
+            0x2001, 0x0db8, 0x85a3, 0x0000, 0x0000, 0x8a2e, 0x0370, 0x7334,
+        ));
 
         assert_eq!(
             extract_indicator(input.as_bytes()),
