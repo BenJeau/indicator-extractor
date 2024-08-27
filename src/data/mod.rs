3291ecde4191db6375f8dffa3679e659d302f1d0
Benoit Jeaurond <benoit@jeaurond.dev>
2024-08-27 17:23:01 -0400

    feat: add simple webscraper

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,6 +1,14 @@
+#[cfg(feature = "pdf")]
 mod pdf;
 
+#[cfg(feature = "pdf")]
 pub use pdf::PdfExtractor;
+
+#[cfg(feature = "website")]
+mod website;
+
+#[cfg(feature = "website")]
+pub use website::WebsiteExtractor;
 
 trait DataExtractor {
     fn extract(&self, data: &[u8]) -> String;
